f7366cc4d7f472f4cfac1a4b12048d658a3a1228
Sturla-ingason <Sturladi@gmail.com>
2026-04-22 16:13:57 +0000

    enable SSL for Render database connection

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,6 +3,6 @@ import { PrismaClient } from './generated/prisma/client/client.ts';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { DATABASE_URL } from '$env/static/private';
 
-const adapter = new PrismaPg({ connectionString: DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: DATABASE_URL, ssl: true });
 const prisma = new PrismaClient({ adapter });
 export default prisma;
